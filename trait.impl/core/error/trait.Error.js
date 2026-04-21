cdb1416c4a5a3507f216139dde9a613868d0ef17
thearossman <thearossman@users.noreply.github.com>
2026-04-21 18:24:12 +0000

    deploy: 96d6796315c54ab4df2aa1068994de4d8506dcc9

---
+++ trait.impl/core/error/trait.Error.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["iris_core",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"iris_core/filter/enum.FilterError.html\" title=\"enum iris_core::filter::FilterError\">FilterError</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[292]}
